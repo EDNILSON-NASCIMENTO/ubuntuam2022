8521dd044ad27e95c165371e37923f1aed904519
EDNILSON-NASCIMENTO <ednilson.nascimento@mail.com>
2022-09-03 13:03:59 -0300

    AULA JAVA 03-09-2022 E HTML

---
+++ java/AULAS/src/Segundo.java
@@ -0,0 +1,19 @@
+
+public class Segundo {
+
+	public static void main(String[] args) {
+		
+		int numero = 0;
+		double numeroQuebrado = 2.15;
+		char caracter = 'c';
+		String palavra = "Oi Mundo!";
+		
+		System.out.println("Ednilson Nascimento");
+		System.out.println("Grace Maria");
+		System.out.println(numero);
+		System.out.println(caracter);
+		System.out.println(palavra);
+		
+	}
+
+}
