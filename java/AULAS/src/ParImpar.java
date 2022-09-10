0df7d8cdcea647bffa73938f63b43df1ac75bd5d
EDNILSON-NASCIMENTO <ednilson.nascimento@mail.com>
2022-09-10 13:15:26 -0300

    novos programas com vetores em java

---
+++ java/AULAS/src/ParImpar.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ParImpar {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		int numero;
+		
+		System.out.println("Digite um numero : ");
+		numero = leia.nextInt();
+		if(numero<0) {
+			System.out.println("Numero negativo");
+		}
+		else if(numero==0) {
+			
+		}
+		else if((numero%2)==0){
+			System.out.println("Numero par");
+		} 
+		else {
+			System.out.println("Numero impar");
+		}
+		
+
+	}
+
+}
