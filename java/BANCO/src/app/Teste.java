e90f31d19ad825a4e25822dda7a5cd4b24dd1d6d
EDNILSON-NASCIMENTO <ednilson.nascimento@mail.com>
2022-10-29 11:13:12 -0300

    AJUSTE APP TESTE E NOVA CLASSE ESTUDANTIL PRONTA

--- java/BANCO/src/app/Teste.java
+++ java/BANCO/src/app/Teste.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 import entities.Corrente;
 import entities.Especial;
+import entities.Estudantil;
 import entities.Poupanca;
 
 public class Teste {
@@ -17,6 +18,8 @@ public class Teste {
 		Poupanca cp1 = new Poupanca(100,1);
 		Corrente cc1 = new Corrente(233,3);
 		Especial ce1 = new Especial(467,1000.00);
+		Estudantil cx1 = new Estudantil(415,5000);
+		
 		char tipo=' ';
 		double valor=0.00;
 		char op=' ';
@@ -25,7 +28,7 @@ public class Teste {
 		int auxDia=0;	
 		
 		System.out.println("ESCOLHA O TIPO DE CONTA");
-		System.out.print("1 - Conta Poupança\n2 - Conta Corrente\n3 - Conta Especial\nDigite opção: ");
+		System.out.print("1 - Conta Poupança\n2 - Conta Corrente\n3 - Conta Especial\n5 - Conta Esdutandil\n6 - Sair\nDigite opção: ");
 		tipoConta = leia.next().charAt(0);
 		if(tipoConta == '1') {
 			System.out.println("MOVIMENTAÇÃO BANCARIA");
@@ -90,7 +93,7 @@ public class Teste {
 
 		}
 		else if (tipoConta == '3') {
-			System.out.println("MOVIMENTAÇÃO BANCARIA");
+			System.out.println("MOVIMENTAÇÃO BANCARIA - CONTA ESPECIAL");
 			for(int x=1; x<=10; x++) {
 				System.out.println("SALDO CONTA R$ "+ce1.getSaldo()+" Limite atual :"+ce1.getLimite());
 				System.out.println("MOVIMENTO :"+x);
@@ -114,6 +117,45 @@ public class Teste {
 				}
 			}
 			System.out.println("Saldo final R$ : "+ce1.getSaldo());
+		}
+		else if (tipoConta == '5') {
+			System.out.println("MOVIMENTAÇÃO BANCARIA - CONTA ESTUDANTIL");
+			for(int x=1; x<=10; x++) {
+				System.out.println("SALDO CONTA R$ "+cx1.getSaldo()+" Limite atual :"+cx1.getLimiteEstudantil());
+				System.out.println("MOVIMENTO :"+x);
+				System.out.print("[D]ébito ou [C]rédito :");
+				tipo = leia.next().toUpperCase().charAt(0);
+				System.out.print("Qual o valor R$ : ");
+				valor = leia.nextDouble();
+				if(tipo=='D') {
+					
+					cx1.debito(valor);
+					
+				} else if(tipo =='C') {
+					cx1.credito(valor);
+				} else {
+					System.out.println("Opção incorreta!!!");
+				}
+				System.out.print("Continua S/N :");
+				op = leia.next().toUpperCase().charAt(0);
+				if(op=='N') {
+					break;
+				}
+			}
+			System.out.println("Saldo atual R$ : "+cx1.getSaldo()+" - Limite R$ "+cx1.getLimiteEstudantil());
+			if(cx1.getLimiteEstudantil()>1000) {
+				System.out.print("Gostaria de solicitar um emprestimo usando o limite ? S/N : ");
+				char opteste = leia.next().toUpperCase().charAt(0);
+				if (opteste=='S') {
+					System.out.print("Valor solicitado R$ : ");
+					double valorteste = leia.nextDouble();
+					cx1.usarLimite(valorteste);
+				}
+			}
+			System.out.println("Saldo final R$ : "+cx1.getSaldo()+" - limite atual R$ :"+cx1.getLimiteEstudantil());
+		}
+		else if (tipoConta=='6') {
+			System.out.println("Até breve...");
 		}
 				
 	}
--- java/BANCO/src/entities/Estudantil.java
+++ java/BANCO/src/entities/Estudantil.java
@@ -1,6 +1,6 @@
 package entities;
 
-public class Estudantil extends Conta{
+public class Estudantil extends Conta {
 
 	private double limiteEstudantil;
 
@@ -26,6 +26,49 @@ public class Estudantil extends Conta{
 	public void setLimiteEstudantil(double limiteEstudantil) {
 		this.limiteEstudantil = limiteEstudantil;
 	}
+	
+	
+	@Override
+	public void debito(double valor) {
+		if(valor<0.00) {
+			System.out.println("Valor negativo, débito não efetuado!");
+		}
+		else if(valor == 0.00) {
+			System.out.println(("Valor zerado, débito não executado!"));
+		}
+		else if(valor > (this.saldo+this.limiteEstudantil)) {
+			System.out.println("Saldo indisponivel");
+		}
+		else if (valor > this.saldo && valor < (this.saldo+this.limiteEstudantil)){
+			System.out.println("Usando valor do limite...");
+			this.limiteEstudantil = (this.saldo+limiteEstudantil)-valor;
+			this.saldo = 0.00;
+		} else {
+			this.saldo = this.saldo - valor;
+		}
+	
+	}
+	
+	public void usarLimite(double valor) {
+		if(valor<0.00) {
+			System.out.println("Valor negativo, credito não efetuado!");
+		}
+		else if(valor == 0.00) {
+			System.out.println(("Valor zerado, credito não executado!"));
+		}
+		else if (limiteEstudantil==0.00) {
+			System.out.println("Você não tem saldo de emprestimo disponivel!!!");
+		}
+		else if(valor> limiteEstudantil) {
+			System.out.println("Valor solicitador maior que o disponivel para emprestimo!!!");
+		}
+		else {
+			this.limiteEstudantil = this.limiteEstudantil-valor;
+			super.credito(valor);
+		}
+	}
+	
+	
 	
 	
 }
